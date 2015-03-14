4243b41026e154d85435f0358e5c31d58a8c99bf
jackrosenhauer <jackrosenhauer@gmail.com>
2015-03-13 22:40:53 -0400

    first

---
+++ src/Model/Main.java
@@ -0,0 +1,10 @@
+package Model;
+
+/**
+ * Created by jackrosenhauer on 3/13/15.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello soylent!");
+    }
+}
